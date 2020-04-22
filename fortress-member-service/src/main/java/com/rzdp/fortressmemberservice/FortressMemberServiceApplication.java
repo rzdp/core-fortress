868fbc0e2b7386bf151beb8ae33b3a7f8f619d9c
rrizpena <rriz.pena@gmail.com>
2020-04-23 02:00:21 +0800

    Added member microservice

---
+++ fortress-member-service/src/main/java/com/rzdp/fortressmemberservice/FortressMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rzdp.fortressmemberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortressMemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortressMemberServiceApplication.class, args);
+    }
+
+}
---
+++ fortress-member-service/src/test/java/com/rzdp/fortressmemberservice/FortressMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzdp.fortressmemberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortressMemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
