1b22f8488b1f92662bc37f0ddca684d63c19b2b3
rrizpena <rriz.pena@gmail.com>
2020-04-24 01:20:51 +0800

    Added new microservice

--- fortress-bank-service/src/main/java/com/rzdp/fortressbankservice/FortressBankServiceApplication.java
+++ fortress-bank-service/src/main/java/com/rzdp/fortressbankservice/FortressBankServiceApplication.java
@@ -1,4 +1,4 @@
-package com.rzdp.fortressmemberservice;
+package com.rzdp.fortressbankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class FortressMemberServiceApplication {
+public class FortressBankServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FortressMemberServiceApplication.class, args);
+        SpringApplication.run(FortressBankServiceApplication.class, args);
     }
 
 }
---
+++ fortress-bank-service/src/test/java/com/rzdp/fortressbankservice/FortressBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzdp.fortressbankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortressBankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
