cb3ef0d1f1e5bfbba2a9e8503b22f9015cfc7b49
rrizpena <rriz.pena@gmail.com>
2020-05-03 03:17:17 +0800

    Fix config

--- fortress-gateway/src/main/java/com/rzdp/fortressgateway/FortressGatewayApplication.java
+++ fortress-gateway/src/main/java/com/rzdp/fortressgateway/FortressGatewayApplication.java
@@ -2,11 +2,12 @@ package com.rzdp.fortressgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEUrek
+@EnableEurekaClient
 public class FortressGatewayApplication {
 
     public static void main(String[] args) {
