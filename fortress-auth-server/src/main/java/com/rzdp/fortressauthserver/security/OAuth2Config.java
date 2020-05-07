b4d7da5701a0d713f1daf291c114ad30b8326730
xrriz <rriz.pena@gmail.com>
2020-05-07 23:48:11 +0800

    Added oauth2 security

--- fortress-auth-server/src/main/java/com/rzdp/fortressauthserver/security/OAuth2Config.java
+++ fortress-auth-server/src/main/java/com/rzdp/fortressauthserver/security/OAuth2Config.java
@@ -1,9 +1,12 @@
 package com.rzdp.fortressauthserver.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -30,7 +33,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient(CLIENT_NAME)
-                .secret(CLIENT_SECRET)
+                .secret(passwordEncoder().encode(CLIENT_SECRET))
                 .authorizedGrantTypes(GRANT_TYPE_REFRESH_TOKEN,
                         GRANT_TYPE_PASSWORD, GRANT_TYPE_CLIENT_CREDENTIALS)
                 .scopes(SCOPE_WEB_CLIENT, SCOPE_MOBILE_CLIENT);
@@ -40,5 +43,10 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- fortress-auth-server/src/main/java/com/rzdp/fortressauthserver/security/WebConfig.java
+++ fortress-auth-server/src/main/java/com/rzdp/fortressauthserver/security/WebConfig.java
@@ -1,18 +1,23 @@
 package com.rzdp.fortressauthserver.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private static final String USER_NAME = "john.carnell";
     private static final String USER_PASSWORD = "password1";
-    private static final String ADMIN_NAME = "willliam.woodward";
+    private static final String ADMIN_NAME = "william.woodward";
     private static final String ADMIN_PASSWORD = "password2";
     private static final String ROLE_USER = "USER";
     private static final String ROLE_ADMIN = "ADMIN";
@@ -21,10 +26,10 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser(USER_NAME)
-                .password(USER_PASSWORD)
+                .password(passwordEncoder.encode(USER_PASSWORD))
                 .roles(ROLE_USER).and()
                 .withUser(ADMIN_NAME)
-                .password(ADMIN_PASSWORD)
+                .password(passwordEncoder.encode(ADMIN_PASSWORD))
                 .roles(ROLE_USER, ROLE_ADMIN);
     }
 
